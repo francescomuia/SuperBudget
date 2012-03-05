b388b0ccd0a69378c353f1cfd3333f8b100ddce1
francescomuia <francescoMuia83@gmail.com>
2012-03-05 09:55:05 +0100

    eclipselink configuration

--- src/test/java/it/superbudget/UserTest.java
+++ src/test/java/it/superbudget/UserTest.java
@@ -1,37 +1,42 @@
 package it.superbudget;
 
 import java.io.InputStream;
+import java.sql.Connection;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import junit.framework.TestCase;
 
-import org.hibernate.ejb.HibernateEntityManager;
+import org.eclipse.persistence.jpa.JpaEntityManager;
 
 public class UserTest extends TestCase
 {
-    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("SuperBudget-test");
+	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("SuperBudget-test");
 
-    public void setUp() throws Exception
-    {
-        InputStream testData = User.class.getResourceAsStream("/user.db.xml");
+	public void setUp() throws Exception
+	{
+		InputStream testData = User.class.getResourceAsStream("/user.db.xml");
 
-        HibernateEntityManager em = (HibernateEntityManager) emf.createEntityManager();
+		JpaEntityManager em = (JpaEntityManager) emf.createEntityManager();
+		Connection connection = em.unwrap(java.sql.Connection.class);
+		System.out.println("CONNECTION " + connection);
+		connection.setAutoCommit(true);
+		System.out.println("CONNECTION " + connection.getAutoCommit());
+		DbUnitDataLoader loader = new DbUnitDataLoader(testData, connection);
 
-        DbUnitDataLoader loader = new DbUnitDataLoader(testData, em.getSession().connection());
+		loader.populateTestData();
+	}
 
-        loader.populateTestData();
-    }
-
-    public void testFindAll()
-    {
-        User.setEntityManager(emf.createEntityManager());
-
-        User user = User.find(1);
-
-        assertNotNull(user);
-        assertEquals(1, user.getId());
-        assertEquals("John Doe", user.getName());
-    }
+	public void testFindAll()
+	{
+		System.out.println("testFindAll");
+		User.setEntityManager(emf.createEntityManager());
+		System.out.println("set EntityManager");
+		User user = User.find(1);
+		System.out.println("user find" + user);
+		assertNotNull(user);
+		assertEquals(1, user.getId());
+		assertEquals("John Doe", user.getName());
+	}
 }
